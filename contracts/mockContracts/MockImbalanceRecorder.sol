caf08171cf4d0bf257ce5ccfe35b2b8569ab1b8c
ilanDoron <ilan.on.the.road@gmail.com>
2017-12-31 00:13:07 +0200

    volume imbalance recorder. fix coding style and write tetst

--- contracts/mockContracts/MockImbalanceRecorder.sol
+++ contracts/mockContracts/MockImbalanceRecorder.sol
@@ -1,7 +1,12 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.4.18;
 
-contract mockImbalanceRecorder {
-    function mockImbalanceRecorder(){
 
+import "../VolumeImbalanceRecorder.sol";
+
+contract MockImbalanceRecorder is VolumeImbalanceRecorder {
+
+    function addTrade( ERC20 token, int buyAmount, uint priceUpdateBlock, uint currentBlock) public {
+
+        addImbalance(token, buyAmount, priceUpdateBlock, currentBlock);
     }
 }
