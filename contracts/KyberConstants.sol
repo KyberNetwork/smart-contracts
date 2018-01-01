834222bffbcd24fe0e0656ec3474c786cdb6c48c
yaron velner <yaron172@yahoo.com>
2018-01-01 14:27:46 +0200

    optimizing structure setting because solidity compiler is stupid

--- contracts/KyberConstants.sol
+++ contracts/KyberConstants.sol
@@ -7,7 +7,7 @@ import "./ERC20Interface.sol";
 
 contract KyberConstants {
 
-    ERC20 constant public ETH_TOKEN_ADDRESS = ERC20(0x00eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee);
+    ERC20 constant ETH_TOKEN_ADDRESS = ERC20(0x00eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee);
     uint  constant PRECISION = (10**18);
     uint  constant MAX_QTY   = (10**28); // 1B tokens
     uint  constant MAX_RATE  = (PRECISION * 10**6); // up to 1M tokens per ETH
