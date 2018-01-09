c86119eba76be810d7e6bd54400d17cfb17bf23d
ilanDoron <ilan.on.the.road@gmail.com>
2018-01-09 11:23:00 +0200

    more liniting

--- contracts/Withdrawable.sol
+++ contracts/Withdrawable.sol
@@ -14,7 +14,7 @@ import "./PermissionGroups.sol";
 contract Withdrawable is PermissionGroups {
 
     event WithdrawToken(ERC20 token, uint amount, address sendTo);
-    
+
     /**
      * @dev Withdraw all ERC20 compatible tokens
      * @param token ERC20 The address of the token contract
