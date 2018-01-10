8d95a520d3e6820bd6842205cf39e4ffeb81098a
ilanDoron <ilan.on.the.road@gmail.com>
2018-01-10 16:38:48 +0200

    set back truffle port to 8545

--- truffle.js
+++ truffle.js
@@ -2,7 +2,7 @@ module.exports = {
   networks: {
     development: {
       host: "localhost",
-      port: 9545,
+      port: 8545,
       gas: 4700000,
       gasPrice: 40000000000,
       network_id: "*" // Match any network id
