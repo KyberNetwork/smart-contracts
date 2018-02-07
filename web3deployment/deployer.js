9b5293c0caeb561285a27d285cfb2dc1ec7a13ee
ilanDoron <ilan.on.the.road@gmail.com>
2018-02-07 17:00:31 +0200

    change caps per user, tester, default, print all outputs to json ouput

--- web3deployment/deployer.js
+++ web3deployment/deployer.js
@@ -198,10 +198,10 @@ function parseInput( jsonInput ) {
     validDurationBlock = web3.utils.toBN(jsonInput["valid duration block"]);
     testers = jsonInput["whitelist params"]["testers"];
     testersCat = jsonInput["whitelist params"]["testers category"];
-    testersCap = jsonInput["whitelist params"]["category cap"];
+    testersCap = jsonInput["whitelist params"]["testers cap"];
     users = jsonInput["whitelist params"]["users"];
     usersCat = jsonInput["whitelist params"]["users category"];
-    usersCap = jsonInput["whitelist params"]["category cap"];
+    usersCap = jsonInput["whitelist params"]["users cap"];
     kgtAddress = jsonInput["whitelist params"]["KGT address"];
 
 
@@ -441,11 +441,20 @@ function printParams(jsonInput) {
     dictOutput["tokens"] = jsonInput.tokens;
     dictOutput["tokens"]["ETH"] = {"name" : "Ethereum", "decimals" : 18, "address" : ethAddress };
     dictOutput["exchanges"] = jsonInput.exchanges;
+    dictOutput["permission"] = jsonInput.permission;
+    dictOutput["whitelist params"] = jsonInput["whitelist params"];
+    dictOutput["max gas price"] = jsonInput["max gas price"];
+    dictOutput["neg diff in bps"] = jsonInput["neg diff in bps"];
+    dictOutput["min expected rate slippage"] = jsonInput["min expected rate slippage"];
+    dictOutput["KNC wallet"] = kncWallet;
+    dictOutput["KNC to ETH rate"] = jsonInput["KNC to ETH rate"];
+    dictOutput["valid duration block"] = jsonInput["valid duration block"];
     dictOutput["reserve"] = reserveAddress;
     dictOutput["pricing"] = conversionRatesAddress;
     dictOutput["network"] = networkAddress;
     dictOutput["wrapper"] = wrapperAddress;
     dictOutput["feeburner"] = feeBurnerAddress;
+
     var json = JSON.stringify(dictOutput, null, 2);
     console.log(json);
     var outputFileName = jsonInput["output filename"];
