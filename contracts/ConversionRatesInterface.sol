3529c5b0ed4ce1e62cb4aaea4ec215eafe816c0e
ilanDoron <ilan.on.the.road@gmail.com>
2018-03-05 15:40:34 +0200

    modify conversion rates interfcase for wrapper

--- contracts/ConversionRatesInterface.sol
+++ contracts/ConversionRatesInterface.sol
@@ -15,4 +15,18 @@ interface ConversionRatesInterface {
         public;
 
     function getRate(ERC20 token, uint currentBlockNumber, bool buy, uint qty) public view returns(uint);
+    function setQtyStepFunction(ERC20 token, int[] xBuy, int[] yBuy, int[] xSell, int[] ySell) public;
+    function setImbalanceStepFunction(ERC20 token, int[] xBuy, int[] yBuy, int[] xSell, int[] ySell) public;
+    function claimAdmin() public;
+    function addOperator(address newOperator) public;
+    function transferAdmin(address newAdmin) public;
+    function addToken(ERC20 token) public;
+    function setTokenControlInfo(
+        ERC20 token,
+        uint minimalRecordResolution,
+        uint maxPerBlockImbalance,
+        uint maxTotalImbalance
+    ) public;
+    function enableTokenTrade(ERC20 token) public;
+    function getTokenControlInfo(ERC20 token) public view returns(uint, uint, uint);
 }
