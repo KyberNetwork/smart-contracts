c7fd95c6aa8a0393722fd8572aa503b4d5fa62cd
ilanDoron <ilan.on.the.road@gmail.com>
2018-03-05 15:05:17 +0200

    base contract for wrapper

---
+++ contracts/wrapperContracts/WrapperBase.sol
@@ -0,0 +1,67 @@
+pragma solidity 0.4.18;
+
+
+
+import "../Withdrawable.sol";
+import "../PermissionGroups.sol";
+
+
+contract WrapperBase is Withdrawable {
+
+    PermissionGroups wrappedContract;
+
+    struct DataTracker {
+        address [] approveSignatureArray;
+        uint lastSetNonce;
+    }
+
+    DataTracker[] internal dataInstances;
+
+    function WrapperBase(PermissionGroups _wrappedContract, address _admin) public {
+        require(_wrappedContract != address(0));
+        require(_admin != address(0));
+        wrappedContract = _wrappedContract;
+        admin = _admin;
+    }
+
+    function claimWrappedContractAdmin() public onlyAdmin {
+        wrappedContract.claimAdmin();
+    }
+
+    function transferWrappedContractAdmin (address newAdmin) public onlyAdmin {
+        wrappedContract.transferAdmin(newAdmin);
+    }
+
+    function addDataInstance() internal returns (uint) {
+        address[] memory add = new address[](0);
+        dataInstances.push(DataTracker(add, 0));
+        return(dataInstances.length - 1);
+    }
+
+    function setNewData(uint dataIndex) internal {
+        require(dataIndex < dataInstances.length);
+        dataInstances[dataIndex].lastSetNonce++;
+        dataInstances[dataIndex].approveSignatureArray.length = 0;
+    }
+
+    function addSignature(uint dataIndex, uint signedNonce, address signer) internal returns(bool allSigned) {
+        require(dataIndex < dataInstances.length);
+        require(dataInstances[dataIndex].lastSetNonce == signedNonce);
+
+        for(uint i = 0; i < dataInstances[dataIndex].approveSignatureArray.length; i++) {
+            if (signer == dataInstances[dataIndex].approveSignatureArray[i]) revert();
+        }
+        dataInstances[dataIndex].approveSignatureArray.push(signer);
+
+        if (dataInstances[dataIndex].approveSignatureArray.length == operatorsGroup.length) {
+            allSigned = true;
+        } else {
+            allSigned = false;
+        }
+    }
+
+    function getDataTrackingParameters(uint index) internal view returns (address[], uint) {
+        require(index < dataInstances.length);
+        return(dataInstances[index].approveSignatureArray, dataInstances[index].lastSetNonce);
+    }
+}
