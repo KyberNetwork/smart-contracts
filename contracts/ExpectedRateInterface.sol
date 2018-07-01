f568bae038d203f9627ace8289e9cbe821a1bc68
ilanDoron <ilan.on.the.road@gmail.com>
2018-07-01 15:38:30 +0300

    Remove unnecessary code change

--- contracts/ExpectedRateInterface.sol
+++ contracts/ExpectedRateInterface.sol
@@ -3,7 +3,6 @@ pragma solidity 0.4.18;
 
 import "./ERC20Interface.sol";
 
-
 interface ExpectedRateInterface {
     function getExpectedRate(ERC20 src, ERC20 dest, uint srcQty) public view
         returns (uint expectedRate, uint slippageRate);
