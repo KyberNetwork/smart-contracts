d603bfb324b7bb564e399b9fe3b0bc5b2bf6c454
ilanDoron <ilan.on.the.road@gmail.com>
2018-07-03 14:33:53 +0300

    fix get decimal safe

--- contracts/Utils2.sol
+++ contracts/Utils2.sol
@@ -25,25 +25,6 @@ contract Utils2 is Utils {
         return decimals[token];
     }
 
-    /// @dev notice, overrides previous implementation.
-    function setDecimals(ERC20 token) internal {
-        uint decimal;
-
-        if (token == ETH_TOKEN_ADDRESS) {
-            decimal = ETH_DECIMALS;
-        } else {
-            if (!address(token).call(bytes4(keccak256("decimals()")))) {/* solhint-disable-line avoid-low-level-calls */
-                //above code can only be performed with low level call. otherwise all operation will revert.
-                // call failed
-                decimal = 18;
-            } else {
-                decimal = token.decimals();
-            }
-        }
-
-        decimals[token] = decimal;
-    }
-
     function calcDestAmount(ERC20 src, ERC20 dest, uint srcAmount, uint rate) internal view returns(uint) {
         return calcDstQty(srcAmount, getDecimals(src), getDecimals(dest), rate);
     }
