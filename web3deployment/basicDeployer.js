c9211c42176fa4bf8100aff682f6b95cdf6d9b96
ilanDoron <ilan.on.the.road@gmail.com>
2018-07-25 12:41:49 +0300

    deploy register wallet in staging and update json

--- web3deployment/basicDeployer.js
+++ web3deployment/basicDeployer.js
@@ -20,7 +20,6 @@ const solc = require('solc')
 const rand = web3.utils.randomHex(7);
 
 const privateKey = web3.utils.sha3("in joy we trust" + rand);
-
 console.log("privateKey", privateKey);
 
 if (printPrivateKey) {
@@ -55,6 +54,9 @@ async function sendTx(txObject) {
     gasLimit = 500 * 1000;
   }
 
+    gasLimit *= 1.2;
+    gasLimit -= gas Limit % 1;
+
   //console.log(gasLimit);
   const txData = txObject.encodeABI();
   const txFrom = account.address;
@@ -81,21 +83,21 @@ async function sendTx(txObject) {
 
 async function deployContract(solcOutput, contractName, ctorArgs) {
 
-  const actualName = contractName;
-  const bytecode = solcOutput.contracts[actualName].bytecode;
+    const actualName = contractName;
+    const bytecode = solcOutput.contracts[actualName].bytecode;
 
-  const abi = solcOutput.contracts[actualName].interface;
-  const myContract = new web3.eth.Contract(JSON.parse(abi));
-  const deploy = myContract.deploy({data:"0x" + bytecode, arguments: ctorArgs});
-  let address = "0x" + web3.utils.sha3(RLP.encode([sender,nonce])).slice(12).substring(14);
-  address = web3.utils.toChecksumAddress(address);
+    const abi = solcOutput.contracts[actualName].interface;
+    const myContract = new web3.eth.Contract(JSON.parse(abi));
+    const deploy = myContract.deploy({data:"0x" + bytecode, arguments: ctorArgs});
+    let address = "0x" + web3.utils.sha3(RLP.encode([sender,nonce])).slice(12).substring(14);
+    address = web3.utils.toChecksumAddress(address);
 
-  await sendTx(deploy);
+    await sendTx(deploy);
 
-  myContract.options.address = address;
+    myContract.options.address = address;
 
 
-  return [address,myContract];
+    return [address,myContract];
 }
 
 const contractPath = path.join(__dirname, "../contracts/");
@@ -124,12 +126,14 @@ const input = {
 //  "Wrapper.sol" : fs.readFileSync(contractPath + 'mockContracts/Wrapper.sol', 'utf8')
   "WrapperBase.sol" : fs.readFileSync(contractPath + 'wrapperContracts/WrapperBase.sol', 'utf8'),
   "WrapFeeBurner.sol" : fs.readFileSync(contractPath + 'wrapperContracts/WrapFeeBurner.sol', 'utf8'),
+  "FeeBurnerWrapperProxy.sol" : fs.readFileSync(contractPath + 'wrapperContracts/FeeBurnerWrapperProxy.sol', 'utf8'),
+  "KyberRegisterWallet.sol" : fs.readFileSync(contractPath + 'wrapperContracts/KyberRegisterWallet.sol', 'utf8')
 };
 
 
 const ethAddress = "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee";
-//const feeBurnerAddress = '0xd6703974Dc30155d768c058189A2936Cf7C62Da6'; //staging
-const feeBurnerAddress = '0xed4f53268bfdff39b36e8786247ba3a02cf34b04';  //production
+const feeBurnerAddress = '0xd6703974Dc30155d768c058189A2936Cf7C62Da6'; //staging
+//const feeBurnerAddress = '0xed4f53268bfdff39b36e8786247ba3a02cf34b04';  //production
 
 async function main() {
     nonce = await web3.eth.getTransactionCount(sender);
@@ -148,19 +152,41 @@ async function main() {
     }
 
 
+    let wrapperFeeBurnerAddress;
+    let wrapFeeBurnerContract;
+
     [wrapperFeeBurnerAddress, wrapFeeBurnerContract] =
         await deployContract(output, "WrapFeeBurner.sol:WrapFeeBurner", [feeBurnerAddress, sender]);
 
+    console.log("wrap fee burner address: " + wrapperFeeBurnerAddress);
+
+    let feeBurnerWrapperProxyAddress;
+    let feeBurnerWrapperProxyContract;
+
+    [feeBurnerWrapperProxyAddress, feeBurnerWrapperProxyContract] =
+        await deployContract(output, "FeeBurnerWrapperProxy.sol:FeeBurnerWrapperProxy", [wrapperFeeBurnerAddress]);
+
+    console.log('feeBurnerWrapperProxyAddress')
+    console.log(feeBurnerWrapperProxyAddress)
+
+    let registerWalletAddress;
+    let registerWalletContract;
+
+    [registerWalletAddress, registerWalletContract] =
+        await deployContract(output, "KyberRegisterWallet.sol:KyberRegisterWallet", [feeBurnerWrapperProxyAddress]);
+
+    console.log('registerWalletAddress')
+    console.log(registerWalletAddress)
 //    console.log("fee burner wrapper");
 //    let wrapperFeeBurnerAddress = '0xBe401c3cf8528DB1B963e2E40827a2E0e1d98Ee4';
 //    let abi = output.contracts["WrapFeeBurner.sol:WrapFeeBurner"].interface;
 //    wrapFeeBurnerContract = await new web3.eth.Contract(JSON.parse(abi), wrapperFeeBurnerAddress);
 
-    console.log("wrap fee burner address: " + wrapperFeeBurnerAddress);
 
-    await sendTx(wrapFeeBurnerContract.methods.addOperator(SpyrosAdd));
-    await sendTx(wrapFeeBurnerContract.methods.addOperator(yaronAdd));
-    await sendTx(wrapFeeBurnerContract.methods.transferAdminQuickly(yaronAdd));
+//    await sendTx(wrapFeeBurnerContract.methods.addOperator(anotherAdd));
+    await sendTx(wrapFeeBurnerContract.methods.addOperator(someAdd));
+    await sendTx(wrapFeeBurnerContract.methods.transferAdminQuickly(someAdd));
+    await sendTx(feeBurnerWrapperProxyContract.methods.transferAdminQuickly(someAdd));
 
     console.log("last nonce is", nonce);
 }
