9fa44783460dca55e0242e00c6eaa5c8faff6d6d
Amit Kotlovski <amitbk@gmail.com>
2018-08-09 23:56:03 +0300

    linkedlist: adding functionality

--- contracts/SortedLinkedList.sol
+++ contracts/SortedLinkedList.sol
@@ -6,16 +6,16 @@ contract SortedLinkedList {
         address maker;
         uint128 srcAmount;
         uint128 dstAmount;
-        uint64 prevOrderId;
-        uint64 nextOrderId;
+        uint64 prevId;
+        uint64 nextId;
     }
 
-    mapping (address => Order) public orders;
+    mapping (uint64 => Order) public orders;
 
     uint64 constant public TAIL_ID = 0;
     uint64 constant public HEAD_ID = 1;
 
-    uint64 nextOrderId = 2;
+    uint64 nextId = 2;
 
     Order internal HEAD;
 
@@ -24,25 +24,35 @@ contract SortedLinkedList {
             maker: 0,
             srcAmount: 0,
             dstAmount: 0,
-            prevOrderId: HEAD_ID,
-            nextOrderId: TAIL_ID
+            prevId: HEAD_ID,
+            nextId: TAIL_ID
         });
 
         orders[HEAD_ID] = HEAD;
     }
 
+    // XXX: remove
+    event DEBUG(uint64 x);
+
     function add(uint128 srcAmount, uint128 dstAmount)
     public
     returns(uint64)
     {
-        uint64 orderId = nextOrderId++;
+        uint64 prevId = nextId - 1;
+        Order storage prevOrder = orders[prevId];
+
+        // Add new order
+        uint64 orderId = nextId++;
         orders[orderId] = Order({
             maker: msg.sender,
             srcAmount: srcAmount,
             dstAmount: dstAmount,
-            prevOrderId: 0,
-            nextOrderId: 0
+            prevId: prevId,
+            nextId: prevOrder.nextId
         });
+
+        // Update previous order
+        prevOrder.nextId = orderId;
 
         return orderId;
     }
@@ -55,16 +65,17 @@ contract SortedLinkedList {
         address _maker,
         uint128 _srcAmount,
         uint128 _dstAmount,
-        uint64 _prevOrderId,
-        uint64 _nextOrderId
+        uint64 _prevId,
+        uint64 _nextId
     )
     {
+        Order storage order = orders[orderId];
         return (
-            0 /* maker */,
-            0 /* srcAmount */,
-            0 /* dstAmount */,
-            0 /* prevOrderId */,
-            0 /* nextOrderId */
+            order.maker,
+            order.srcAmount,
+            order.dstAmount,
+            order.prevId,
+            order.nextId
         );
     }
 }
--- test/sortedlinkedlist.js
+++ test/sortedlinkedlist.js
@@ -13,7 +13,8 @@ const SortedLinkedList = artifacts.require("SortedLinkedList");
 contract('SortedLinkedList test', async (accounts) => {
 
     beforeEach('setup contract for each test', async () => {
-        user = accounts[0];
+        user1 = accounts[0];
+        user2 = accounts[1];
         list = await SortedLinkedList.new();
     });
 
@@ -33,32 +34,128 @@ contract('SortedLinkedList test', async (accounts) => {
         tailId.should.be.bignumber.equal(0);
     });
 
-    it("should add head with tail as nextId", async () => {
-        let headId = await list.HEAD_ID();
-        let tailId = await list.TAIL_ID();
-        let head = await list.getOrderDetails(headId);
-        let [maker, srcAmount, dstAmount, prevId, nextId] = head;
+    it("head should initially point to tail as its nextId", async () => {
+        let head = await getOrderById(await list.HEAD_ID());
 
-        maker.should.be.bignumber.equal(0);
-        srcAmount.should.be.bignumber.equal(0);
-        dstAmount.should.be.bignumber.equal(0);
-        nextId.should.be.bignumber.equal(tailId);
-        prevId.should.be.bignumber.equal(0);
+        head.nextId.should.be.bignumber.equal(await list.TAIL_ID());
     });
 
-    xit("should allow adding new order", async () => {
-        let orderId = await list.add(
-            10 /*srcAmount*/,
-            100 /*dstAmount*/);
+    it("should add order with unique id", async () => {
+        let orderId = await list.add.call(
+            10 /* srcAmount */,
+            100 /* dstAmount */);
 
-        let res = await list.getOrderDetails(0);
-        // let [maker, srcAmount, dstAmount, nextId, prevId] = res
-        // assert.equal(this.user, maker);
+        orderId.should.be.bignumber.not.equal(await list.HEAD_ID());
+        orderId.should.be.bignumber.not.equal(await list.TAIL_ID());
     });
+
+    it("should add order and get its data back with user as maker", async () => {
+        let order = await addOrder(10 /* srcAmount */, 100 /* dstAmount */);
+
+        order.maker.should.equal(user1);
+        order.srcAmount.should.be.bignumber.equal(10);
+        order.dstAmount.should.be.bignumber.equal(100);
+    });
+
+    it("should add single order so that head is its prev and tail is its next", async () => {
+        let order = await addOrder(10 /* srcAmount */, 100 /* dstAmount */);
+
+        order.prevId.should.be.bignumber.equal(await list.HEAD_ID());
+        order.nextId.should.be.bignumber.equal(await list.TAIL_ID());
+    });
+
+    it("should add two orders and get the data back with users as makers", async () => {
+        let order1 = await addOrder(
+            10 /* srcAmount */,
+            100 /* dstAmount */,
+            {from: user1}
+        );
+        let order2 = await addOrder(
+            10 /* srcAmount */,
+            200 /* dstAmount */,
+            {from: user2}
+        );
+
+        order1.maker.should.equal(user1);
+        order1.srcAmount.should.be.bignumber.equal(10);
+        order1.dstAmount.should.be.bignumber.equal(100);
+        order2.maker.should.equal(user2);
+        order2.srcAmount.should.be.bignumber.equal(10);
+        order2.dstAmount.should.be.bignumber.equal(200);
+    });
+
+    it("should add two orders so that -> head <-> first <-> second <-> tail", async () => {
+        let id1 = await addOrderGetId(10 /* srcAmount */, 100 /* dstAmount */);
+        let id2 = await addOrderGetId(10 /* srcAmount */, 200 /* dstAmount */);
+
+        let head = await getOrderById(await list.HEAD_ID());
+        let order1 = await getOrderById(id1)
+        let order2 = await getOrderById(id2)
+        // head -> 1 -> 2 -> tail
+        head.nextId.should.be.bignumber.equal(order1.id);
+        order1.nextId.should.be.bignumber.equal(order2.id);
+        order2.nextId.should.be.bignumber.equal(await list.TAIL_ID());
+        // head <- 1 <- 2
+        order2.prevId.should.be.bignumber.equal(order1.id);
+        order1.prevId.should.be.bignumber.equal(head.id);
+    });
+
+    it("should add orders according to sorting algorithm", async () => {
+        let worseId = await addOrderGetId(
+            10 /* srcAmount */,
+            100 /* dstAmount */
+        );
+        let betterId = await addOrderGetId(
+            10 /* srcAmount */,
+            200 /* dstAmount */
+        );
+
+        let head = await getOrderById(await list.HEAD_ID());
+        let worse = await getOrderById(worseId);
+        let better = await getOrderById(betterId);
+        // head -> better -> worse -> tail
+        head.nextId.should.be.bignumber.equal(better.id);
+        better.nextId.should.be.bignumber.equal(worse.id);
+        worse.nextId.should.be.bignumber.equal(await list.TAIL_ID());
+        // head <- better <- worse
+        worse.prevId.should.be.bignumber.equal(better.id);
+        better.prevId.should.be.bignumber.equal(head.id);
+    });
+
+    // TODO: Add order after specified order
+    // TODO: Add an order as one user and check make from another user
+    // TODO: Do not allow removing / updating HEAD
+    // TODO: Only allow maker or admin to remove / update order
 
 });
 
+class Order {
+    constructor(id, maker, srcAmount, dstAmount, prevId, nextId) {
+        this.id = id;
+        this.maker = maker;
+        this.srcAmount = srcAmount;
+        this.dstAmount = dstAmount;
+        this.prevId = prevId;
+        this.nextId = nextId;
+    }
+}
 
-function log (string) {
-    console.log(string);
-};
+async function getOrderById(id) {
+    let params = await list.getOrderDetails(id);
+    let [maker, srcAmount, dstAmount, prevId, nextId] = params;
+    return new Order(id, maker, srcAmount, dstAmount, prevId, nextId);
+}
+
+async function addOrderGetId(srcAmount, dstAmount, args = {}) {
+    // "Calling" the contract's add function does not return the id value so
+    // we first run add.call() to perform the action without changing the state
+    // of the blockchain, then actually running add to make the changes.
+    let orderId = await list.add.call(srcAmount, dstAmount, args);
+    await list.add(srcAmount, dstAmount, args);
+    return orderId;
+}
+
+async function addOrder(srcAmount, dstAmount, args = {}) {
+    let orderId = await addOrderGetId(srcAmount, dstAmount, args);
+    return await getOrderById(orderId);
+}
