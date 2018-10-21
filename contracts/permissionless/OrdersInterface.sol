13bfc5a7a56cebed00ff4cd71f4572232c4aeae6
ilanDoron <ilan.on.the.road@gmail.com>
2018-10-21 16:00:54 +0300

    add return value to orders.sol

--- contracts/permissionless/OrdersInterface.sol
+++ contracts/permissionless/OrdersInterface.sol
@@ -3,11 +3,11 @@ pragma solidity 0.4.18;
 
 interface OrdersInterface {
     function getOrderDetails(uint32 orderId) public view returns (address, uint128, uint128, uint32, uint32);
-    function add(address maker, uint32 orderId, uint128 srcAmount, uint128 dstAmount) public;
+    function add(address maker, uint32 orderId, uint128 srcAmount, uint128 dstAmount) public returns (bool);
     function addAfterId(address maker, uint32 orderId, uint128 srcAmount, uint128 dstAmount, uint32 prevId) public
         returns (bool);
-    function removeById(uint32 orderId) public;
-    function update(uint32 orderId, uint128 srcAmount, uint128 dstAmount) public;
+    function removeById(uint32 orderId) public returns (bool);
+    function update(uint32 orderId, uint128 srcAmount, uint128 dstAmount) public returns (bool);
     function updateWithPositionHint(uint32 orderId, uint128 srcAmount, uint128 dstAmount, uint32 prevId) public
         returns(bool, uint);
     function getFirstOrder() public view returns(uint32 orderId, bool isEmpty);
