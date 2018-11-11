82d2218ad8a5731e3147479d24a7dda31f63e6e1
Amit Kotlovski <amitbk@gmail.com>
2018-11-11 09:13:00 +0200

    Orders: removing unused function

--- contracts/permissionless/OrdersInterface.sol
+++ contracts/permissionless/OrdersInterface.sol
@@ -12,7 +12,6 @@ interface OrdersInterface {
         returns(bool, uint);
     function getFirstOrder() public view returns(uint32 orderId, bool isEmpty);
     function allocateIds(uint32 howMany) public returns(uint32);
-    function subSrcAndDstAmounts(uint32 orderId, uint128 subFromSrc) public returns (uint128 _subDst);
     function getTailId() public view returns(uint32);
     function getHeadId() public view returns(uint32);
     function findPrevOrderId(uint128 srcAmount, uint128 dstAmount) public view returns(uint32);
