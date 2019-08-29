a96d3eede6995dd51d4861ac0f425b6b658f0034
Desmond Ho <howeilun@hotmail.com>
2019-08-29 14:17:53 +0800

    Buidler integration, test fixes

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-This repository contains kyber network swap smart contracts.
+## Introduction
+This repository contains kyber network smart contracts.
 For more details, please visit our [developer portal](https://developer.kyber.network/)
+
+## Testing with Buidler
+1. `npm i sha3 --save`
+2. `npm i`
+3. `node --max-old-space-size=4096 node_modules/.bin/buidler compile --config buidlerConfig.js`
+4. Run `ganache-cli -e 1000` in another terminal
+5. `npx buidler test --no-compile --config buidlerConfig.js`
---
+++ contracts/mockContracts/MockConversionRate2.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.4.18;
+
+
+import "../ConversionRates2.sol";
+
+contract MockConversionRate2 is ConversionRates2 {
+
+    function MockConversionRate2(address admin) ConversionRates2(admin) public {
+
+    }
+
+    function mockGetMaxTotalImbalance(ERC20 token) public view returns(uint) {
+        return getMaxTotalImbalance(token);
+    }
+
+    function getUpdateRateBlockFromCompact (ERC20 token) public view returns(uint updateRateBlock) {
+        // get rate update block
+        bytes32 compactData = tokenRatesCompactData[tokenData[token].compactDataArrayIndex];
+        updateRateBlock = getLast4Bytes(compactData);
+    }
+
+    // this for testing execute step func, only use imbalance step as qty can be negative 
+    function mockExecuteStepFunction(ERC20 token, int qty) public view returns (int) {
+        return executeStepFunction(tokenData[token].buyRateImbalanceStepFunction, qty);
+    }
+
+    function mockAddBps(uint rate, int bps) public pure returns(uint) {
+        return addBps(rate, bps);
+    }
+}
