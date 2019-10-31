621169f6089e5acbd1569fdbcc3771a35edeb2b1
Le Xuan Manh <manhlx.300695@gmail.com>
2019-10-31 15:48:04 +0700

    [WIP] Eth2Dai Reserve enhancement  (#449)
    * Add eth2dai contract
    
    * Update Eth2Dai reserve
    
    * Update Eth2DaiReserve contract and tests
    
    * Update trade function tests for eth2dai reserve
    
    * Make single test can run on its own
    
    * Separate source codes, add some v5 contracts
    
    * Update contracts fixed some review comments, remove some unused functions
    
    * Update eth2dai contract with enable/disable internal inventory for token
    
    * Add test for set internal inventory enable/disable
    
    * Put enable/disable internal inventory into internal inventory data
    
    * Add internal inventory tests
    
    * Change overlow to out of range

---
+++ contractsV5/WithdrawableV5.sol
@@ -0,0 +1,29 @@
+pragma solidity 0.5.11;
+
+import "./ERC20InterfaceV5.sol";
+import "./PermissionGroupsV5.sol";
+
+
+contract Withdrawable is PermissionGroups {
+
+    event TokenWithdraw(ERC20 token, uint amount, address sendTo);
+
+    /**
+     * @dev Withdraw all ERC20 compatible tokens
+     * @param token ERC20 The address of the token contract
+     */
+    function withdrawToken(ERC20 token, uint amount, address sendTo) external onlyAdmin {
+        require(token.transfer(sendTo, amount));
+        emit TokenWithdraw(token, amount, sendTo);
+    }
+
+    event EtherWithdraw(uint amount, address sendTo);
+
+    /**
+     * @dev Withdraw Ethers
+     */
+    function withdrawEther(uint amount, address payable sendTo) external onlyAdmin {
+        sendTo.transfer(amount);
+        emit EtherWithdraw(amount, sendTo);
+    }
+}
