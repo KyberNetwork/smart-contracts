cb97a93a8a2811178966fca1111f433f3e351c2e
Desmond Ho <howeilun@hotmail.com>
2019-11-08 18:40:36 +0800

    Upgraded to ganache-cli@istanbul

--- tst.sh
+++ tst.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
-while getopts "f:" arg; do
+while getopts "f:k:" arg; do
   case $arg in
     f) FILE=$OPTARG;;
+    k) FORK=$OPTARG;;
   esac
 done
 
-npx ganache-cli -e 1000 -q & 2> /dev/null
+if [ -n "$FORK" ]
+then
+  npx ganache-cli -e 1000 -q & 2> /dev/null
+else
+  npx ganache-cli -e 1000 -k $FORK -q & 2> /dev/null
+fi
+
 pid=$!
 sleep 3
 if [ -n "$FILE" ]
