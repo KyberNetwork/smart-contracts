a193515bbf0d661343ea32a67aecd2777b55e099
Desmond Ho <howeilun@hotmail.com>
2019-11-11 22:57:58 +0800

    Revert "Merge branch 'clicky/ganacheIstanbul' into development"
    This reverts commit 654a1c3e7971319bd802df0cd70daf32337caa95, reversing
    changes made to 6c0a21c59eb55b44cfe8616a513e7e76533fc9c5.

--- tst.sh
+++ tst.sh
@@ -1,23 +1,11 @@
 #!/bin/sh
-mag=$'\e[1;35m'
-cyn=$'\e[1;36m'
-end=$'\e[0m'
-
-while getopts "f:k:" arg; do
+while getopts "f:" arg; do
   case $arg in
     f) FILE=$OPTARG;;
-    k) FORK=$OPTARG;;
   esac
 done
 
-if [ -n "$FORK" ] 
-then
-  printf "%s\n\n" "${mag} Running fork:${end} ${cyn}$FORK${end}"
-  npx ganache-cli -e 1000 -k $FORK -q & 2> /dev/null
-else
-  npx ganache-cli -e 1000 -q & 2> /dev/null
-fi
-
+npx ganache-cli -e 1000 -q & 2> /dev/null
 pid=$!
 sleep 3
 if [ -n "$FILE" ]
