96a3eaf8a3263ad8a6285acc63a48a8db42713bf
Ilan Doron <ilan.on.the.road@gmail.com>
2019-12-23 09:55:59 +0200

    upgrade kyber weth reserve to web3

--- test/v4/kyberWethReserve.js
+++ test/v4/kyberWethReserve.js
@@ -1,11 +1,4 @@
-const Web3 = require('web3');
-const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
-const BigNumber = web3.BigNumber
-
-require("chai")
-    .use(require("chai-as-promised"))
-    .use(require('chai-bignumber')(BigNumber))
-    .should()
+const BN = web3.utils.BN;
 
 const MockWeth = artifacts.require("./wethContracts/mockContracts/WETH9.sol");
 const KyberWethReserve = artifacts.require("./wethContracts/KyberWethReserve");
@@ -14,7 +7,10 @@ const TestToken = artifacts.require("./mockContracts/TestToken.sol");
 const Helper = require("./helper.js");
 
 const ethAddress = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE';
-const precision = new BigNumber(10).pow(18);
+const zeroAddress = '0x0000000000000000000000000000000000000000';
+const zeroBN = new BN(0);
+const precision = new BN(10).pow(new BN(18));
+const eth1 = new BN(10).pow(new BN(18));
 
 contract('KyberWethReserve', function (accounts) {
 
@@ -22,8 +18,8 @@ contract('KyberWethReserve', function (accounts) {
     const non_admin = accounts[1]
     const alerter = accounts[2];
 
-    const amount = new BigNumber(10).pow(18).mul(3); // 3 eth
-    const tweiSrcQty = new BigNumber(10).pow(18).mul(0.2); // 0.2 eth
+    const amount3Eth = precision.mul(new BN(3)); // 3 eth
+    const tweiSrcQty = eth1.div(new BN(5)); // 0.2 eth
     const expectedRate = precision;
 
     beforeEach('create contracts', async function () {
@@ -35,101 +31,97 @@ contract('KyberWethReserve', function (accounts) {
         reserve = await KyberWethReserve.new(admin, wethToken.address, admin);
 
         // buy some weth to start with
-        const initialBuyRate = await reserve.getConversionRate(ethAddress, wethToken.address, amount, 0);
-        await reserve.trade(ethAddress, amount, wethToken.address, admin, initialBuyRate, true, {value: amount});
+        const initialBuyRate = await reserve.getConversionRate(ethAddress, wethToken.address, amount3Eth, 0);
+        await reserve.trade(ethAddress, amount3Eth, wethToken.address, admin, initialBuyRate, true, {value: amount3Eth});
 
         // approve big quantity
-        await wethToken.approve(reserve.address, amount.mul(1000));
+        await wethToken.approve(reserve.address, amount3Eth.mul(new BN(1000)));
     });
 
     describe('as admin', function () {
         it("check expected eth->weth rate", async function (){
-            const buyRate = await reserve.getConversionRate(ethAddress, wethToken.address, amount, 0);
-            buyRate.valueOf().should.equal(expectedRate.valueOf());
+            const buyRate = await reserve.getConversionRate(ethAddress, wethToken.address, amount3Eth, 0);
+            Helper.assertEqual(buyRate, expectedRate);
         });
         
         it("check wei lost after eth->weth trade", async function (){
-            const buyRate = await reserve.getConversionRate(ethAddress, wethToken.address, amount, 0);
+            const buyRate = await reserve.getConversionRate(ethAddress, wethToken.address, amount3Eth, 0);
             const balanceBefore = await Helper.getBalancePromise(admin);
 
-            const txInfo = await reserve.trade(ethAddress, amount, wethToken.address, admin, buyRate, true, {value: amount});
-            const tx = await web3.eth.getTransaction(txInfo.tx);
-            const gasCost = tx.gasPrice.mul(txInfo.receipt.gasUsed);
+            const txInfo = await reserve.trade(ethAddress, amount3Eth, wethToken.address, admin, buyRate, true, {value: amount3Eth});
+            const gasCost = await calcGasCost(txInfo);
 
             const balanceAfter = await Helper.getBalancePromise(admin);
-            const weiLost = balanceBefore.minus(balanceAfter)
+            const weiLost = balanceBefore.sub(balanceAfter)
 
-            assert.equal(weiLost.valueOf(), amount.plus(gasCost).valueOf(), "wrong wei amount lost in the trade")
+            Helper.assertEqual(weiLost, amount3Eth.add(gasCost), "wrong wei amount lost in the trade")
         });
 
         it("check wei lost after eth->weth trade", async function (){
-            const buyRate = await reserve.getConversionRate(ethAddress, wethToken.address, amount, 0);
+            const buyRate = await reserve.getConversionRate(ethAddress, wethToken.address, amount3Eth, 0);
             const balanceBefore = await Helper.getBalancePromise(admin);
 
-            const txInfo = await reserve.trade(ethAddress, amount, wethToken.address, admin, buyRate, true, {value: amount});
-            const tx = await web3.eth.getTransaction(txInfo.tx);
-            const gasCost = tx.gasPrice.mul(txInfo.receipt.gasUsed);
+            const txInfo = await reserve.trade(ethAddress, amount3Eth, wethToken.address, admin, buyRate, true, {value: amount3Eth});
+            const gasCost = await calcGasCost(txInfo);
 
             const balanceAfter = await Helper.getBalancePromise(admin);
-            const weiLost = balanceBefore.minus(balanceAfter)
+            const weiLost = balanceBefore.sub(balanceAfter)
 
-            assert.equal(weiLost.valueOf(), amount.plus(gasCost).valueOf(), "wrong wei amount lost in the trade")
+            Helper.assertEqual(weiLost, amount3Eth.add(gasCost), "wrong wei amount3Eth lost in the trade")
         });
 
         it("check twei gained after eth->weth trade", async function (){
-            const buyRate = await reserve.getConversionRate(ethAddress, wethToken.address, amount, 0);
+            const buyRate = await reserve.getConversionRate(ethAddress, wethToken.address, amount3Eth, 0);
             const tweiBalanceBefore = await wethToken.balanceOf(admin);
 
-            await reserve.trade(ethAddress, amount, wethToken.address, admin, buyRate, true, {value: amount});
+            await reserve.trade(ethAddress, amount3Eth, wethToken.address, admin, buyRate, true, {value: amount3Eth});
             const tweiBalanceAfter = await wethToken.balanceOf(admin);
-            const tWeiGained = tweiBalanceAfter.minus(tweiBalanceBefore)
-            const expecetedTweiGained = new BigNumber(expectedRate).mul(amount).div(precision)
+            const tWeiGained = tweiBalanceAfter.sub(tweiBalanceBefore)
+            const expecetedTweiGained = (new BN(expectedRate)).mul(amount3Eth).div(precision)
 
-            assert.equal(tWeiGained.valueOf(), expecetedTweiGained.valueOf(), "wrong expected token wei gained")
+            Helper.assertEqual(tWeiGained, expecetedTweiGained, "wrong expected token wei gained")
         });
 
         it("check reserve did not gain tokens after eth->weth trade", async function (){
-            const buyRate = await reserve.getConversionRate(ethAddress, wethToken.address, amount, 0);
+            const buyRate = await reserve.getConversionRate(ethAddress, wethToken.address, amount3Eth, 0);
 
             const reserveTweiBalanceBefore = await wethToken.balanceOf(reserve.address);
-            await reserve.trade(ethAddress, amount, wethToken.address, admin, buyRate, true, {value: amount});
+            await reserve.trade(ethAddress, amount3Eth, wethToken.address, admin, buyRate, true, {value: amount3Eth});
 
             const reserveTweiBalanceAfter = await wethToken.balanceOf(reserve.address);
-            const reserveTweiGained = reserveTweiBalanceAfter.minus(reserveTweiBalanceBefore)
+            const reserveTweiGained = reserveTweiBalanceAfter.sub(reserveTweiBalanceBefore)
 
-            assert.equal(reserveTweiGained.valueOf(), 0, "wrong token wei gained by reserve")
+            Helper.assertEqual(reserveTweiGained, zeroBN, "wrong token wei gained by reserve")
         });
 
         it("check reserve did not gain eth after eth->weth trade", async function (){
-            const buyRate = await reserve.getConversionRate(ethAddress, wethToken.address, amount, 0);
+            const buyRate = await reserve.getConversionRate(ethAddress, wethToken.address, amount3Eth, 0);
 
             const reserveWeiBalanceBefore = await Helper.getBalancePromise(reserve.address);
-            await reserve.trade(ethAddress, amount, wethToken.address, admin, buyRate, true, {value: amount});
+            await reserve.trade(ethAddress, amount3Eth, wethToken.address, admin, buyRate, true, {value: amount3Eth});
 
             const reserveWeiBalanceAfter = await Helper.getBalancePromise(reserve.address);
-            const reserveWeiGained = reserveWeiBalanceAfter.minus(reserveWeiBalanceBefore)
+            const reserveWeiGained = reserveWeiBalanceAfter.sub(reserveWeiBalanceBefore)
 
-            assert.equal(reserveWeiGained.valueOf(), 0, "wrong token wei gained by reserve")
+            Helper.assertEqual(reserveWeiGained, zeroBN, "wrong token wei gained by reserve")
         });
 
         it("check expected weth->eth rate", async function (){
             const sellRate = await reserve.getConversionRate(wethToken.address, ethAddress, tweiSrcQty, 0);
-            sellRate.valueOf().should.equal(expectedRate.valueOf());
+            Helper.assertEqual(sellRate, expectedRate);
         });
 
         it("check wei gained in weth->eth trade", async function (){
             const sellRate = await reserve.getConversionRate(wethToken.address, ethAddress, tweiSrcQty, 0);
             const balanceBefore = await Helper.getBalancePromise(admin);
-
             const txInfo = await reserve.trade(wethToken.address, tweiSrcQty, ethAddress, admin, sellRate, true);
-            const tx = await web3.eth.getTransaction(txInfo.tx);
-            const gasCost = tx.gasPrice.mul(txInfo.receipt.gasUsed);
+            const gasCost = await calcGasCost(txInfo);
 
             const balanceAfter = await Helper.getBalancePromise(admin);
-            const weiGained = balanceAfter.minus(balanceBefore)
-            const expecetedWeiGained = new BigNumber(tweiSrcQty).mul(expectedRate).div(precision)
+            const weiGained = balanceAfter.sub(balanceBefore)
+            const expecetedWeiGained = tweiSrcQty.mul(expectedRate).div(precision)
 
-            assert.equal(weiGained.valueOf(), expecetedWeiGained.minus(gasCost).valueOf(), "wrong expected wei gained");
+            Helper.assertEqual(weiGained, expecetedWeiGained.sub(gasCost), "wrong expected wei gained");
         });
 
         it("check twei lost in weth->eth trade", async function (){
@@ -138,9 +130,9 @@ contract('KyberWethReserve', function (accounts) {
 
             await reserve.trade(wethToken.address, tweiSrcQty, ethAddress, admin, sellRate, true);
             const tweiBalanceAfter = await wethToken.balanceOf(admin);
-            const tWeiLost = tweiBalanceBefore.minus(tweiBalanceAfter)
+            const tWeiLost = tweiBalanceBefore.sub(tweiBalanceAfter)
 
-            assert.equal(tWeiLost.valueOf(), tweiSrcQty.valueOf(), "wrong expected token wei lost")
+            Helper.assertEqual(tWeiLost, tweiSrcQty, "wrong expected token wei lost")
         });
 
         it("check reserve did not gain wei in a weth->eth trade", async function (){
@@ -150,9 +142,9 @@ contract('KyberWethReserve', function (accounts) {
 
             await reserve.trade(wethToken.address, tweiSrcQty, ethAddress, admin, sellRate, true);
             const reserveBalanceAfter = await Helper.getBalancePromise(reserve.address);
-            const reserveWeiGained = reserveBalanceAfter.minus(reserveBalanceBefore)
+            const reserveWeiGained = reserveBalanceAfter.sub(reserveBalanceBefore)
 
-            assert.equal(reserveWeiGained.valueOf(), 0, "wrong reserve wei gained in the trade");
+            Helper.assertEqual(reserveWeiGained, 0, "wrong reserve wei gained in the trade");
         });
 
         it("check reserve did not gain tokens after eth->weth trade", async function (){
@@ -162,28 +154,28 @@ contract('KyberWethReserve', function (accounts) {
             await reserve.trade(wethToken.address, tweiSrcQty, ethAddress, admin, sellRate, true);
 
             const reserveTweiBalanceAfter = await wethToken.balanceOf(reserve.address);
-            const reserveTweiGained = reserveTweiBalanceAfter.minus(reserveTweiBalanceBefore)
+            const reserveTweiGained = reserveTweiBalanceAfter.sub(reserveTweiBalanceBefore)
 
-            assert.equal(reserveTweiGained.valueOf(), 0, "wrong token wei gained by reserve")
+            Helper.assertEqual(reserveTweiGained, 0, "wrong token wei gained by reserve")
         });
 
         it("check eth->weth price for 0 quantity", async function (){
             const buyRate = await reserve.getConversionRate(ethAddress, wethToken.address, 0, 0);
-            buyRate.valueOf().should.equal(expectedRate.valueOf());
+            Helper.assertEqual(buyRate, expectedRate);
         });
 
         it("check weth->eth price for 0 quantity", async function (){
             const sellRate = await reserve.getConversionRate(wethToken.address, ethAddress, 0, 0);
-            sellRate.valueOf().should.equal(expectedRate.valueOf());
+            Helper.assertEqual(sellRate, expectedRate);
         });
 
         it("disable trades in the reserve and see that trade reverts", async function (){
             await reserve.addAlerter(alerter);
             await reserve.disableTrade({from: alerter});
 
-            const buyRate = await reserve.getConversionRate(ethAddress, wethToken.address, amount, 0);
+            const buyRate = await reserve.getConversionRate(ethAddress, wethToken.address, amount3Eth, 0);
             await Helper.expectThrow(
-                    reserve.trade(ethAddress, amount, wethToken.address, admin, buyRate, true, {value: amount})
+                    reserve.trade(ethAddress, amount3Eth, wethToken.address, admin, buyRate, true, {value: amount3Eth})
             );
 
             await reserve.enableTrade();
@@ -191,24 +183,24 @@ contract('KyberWethReserve', function (accounts) {
 
         it("change network to another address and see that trade fails since sender is not network", async function (){
             await reserve.setKyberNetwork(accounts[2]);
-            const buyRate = await reserve.getConversionRate(ethAddress, wethToken.address, amount, 0);
+            const buyRate = await reserve.getConversionRate(ethAddress, wethToken.address, amount3Eth, 0);
             await Helper.expectThrow(
-                    reserve.trade(ethAddress, amount, wethToken.address, admin, buyRate, true, {value: amount})
+                    reserve.trade(ethAddress, amount3Eth, wethToken.address, admin, buyRate, true, {value: amount3Eth})
             );
         });
 
         it("see getconversionrate without weth fails", async function () {
-            const buyRate = await reserve.getConversionRate(ethAddress, anotherToken.address, amount, 0);
-            assert.equal(buyRate, 0);
+            const buyRate = await reserve.getConversionRate(ethAddress, anotherToken.address, amount3Eth, 0);
+            Helper.assertEqual(buyRate, zeroBN);
         });
 
         it("see getconversionrate without eth fails", async function (){
-            const buyRate = await reserve.getConversionRate(anotherToken.address, wethToken.address, amount, 0);
-            assert.equal(buyRate, 0);
+            const buyRate = await reserve.getConversionRate(anotherToken.address, wethToken.address, amount3Eth, 0);
+            Helper.assertEqual(buyRate, zeroBN);
         });
 
         it("try to send eth to reserve and make sure default payable function throws", async function (){
-            let weiSrcQty = new BigNumber(10).pow(18); // 1 eth
+            let weiSrcQty = eth1; // 1 eth
 
             // make sure can send to another account 
             Helper.sendEtherWithPromise(accounts[8], accounts[0], weiSrcQty)
@@ -220,9 +212,9 @@ contract('KyberWethReserve', function (accounts) {
     describe('as non admin', function () {
         const from = non_admin;
         it("check trade fails", async function (){
-            const buyRate = await reserve.getConversionRate(ethAddress, wethToken.address, amount, 0);
+            const buyRate = await reserve.getConversionRate(ethAddress, wethToken.address, amount3Eth, 0);
             await Helper.expectThrow(
-                reserve.trade(ethAddress, amount, wethToken.address, admin, buyRate, true, {value: amount}, {from})
+                reserve.trade(ethAddress, amount3Eth, wethToken.address, admin, buyRate, true, {value: amount3Eth, from})
             );
         });
 
@@ -232,3 +224,13 @@ contract('KyberWethReserve', function (accounts) {
     });
 
 });
+
+async function calcGasCost(txInfo) {
+    let tx = await web3.eth.getTransaction(txInfo.tx);
+    return (new BN(tx.gasPrice)).mul(new BN(txInfo.receipt.gasUsed));
+}
+
+
+function log(str) {
+    console.log(str);
+}
