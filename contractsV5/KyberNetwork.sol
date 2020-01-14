4d34d01e41724b133684d509fee45132ba4a0b38
Desmond Ho <howeilun@hotmail.com>
2020-01-14 17:12:24 +0800

    Finetune searchBestRate and findRatesAndAmounts, hint parser

--- contractsV5/KyberNetwork.sol
+++ contractsV5/KyberNetwork.sol
@@ -433,7 +433,6 @@ contract KyberNetwork is Withdrawable, Utils, IKyberNetwork, ReentrancyGuard {
         for (uint i = 0; i < reserveArr.length; i++) {
 
             if (i == bestReserve.index) continue;
-
             isPayingFees = isFeePayingReserve[address(reserve)];
             srcAmountWithFee = ((src == ETH_TOKEN_ADDRESS) && isPayingFees) ? srcAmount - takerFee : srcAmount;
             destAmount = srcAmountWithFee * rates[i] / PRECISION;
