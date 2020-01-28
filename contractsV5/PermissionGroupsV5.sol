f149087f1edc2a2a6f8803a551876ce8fb9c2a3c
Ilan Doron <ilan.on.the.road@gmail.com>
2020-01-28 15:39:47 +0200

    fix revert message for transfer admin quickly

--- contractsV5/PermissionGroupsV5.sol
+++ contractsV5/PermissionGroupsV5.sol
@@ -56,7 +56,7 @@ contract PermissionGroups {
      * @param newAdmin The address to transfer ownership to.
      */
     function transferAdminQuickly(address newAdmin) public onlyAdmin {
-        require(newAdmin != address(0), "Operator address is 0");
+        require(newAdmin != address(0), "transferAdminQuickly: address 0");
         emit TransferAdminPending(newAdmin);
         emit AdminClaimed(newAdmin, admin);
         admin = newAdmin;
