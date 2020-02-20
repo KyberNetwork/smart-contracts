432f188f3f565ea6d9932e7ac5b969dd120719f1
Mike <manhlx.300695@gmail.com>
2020-02-20 18:49:20 +0700

    Add Staking and DAO contracts + tests

---
+++ contractsV5/stakingAndDao/mock/MockKyberDaoWithdrawFailed.sol
@@ -0,0 +1,18 @@
+pragma solidity 0.5.11;
+
+import "../EpochUtils.sol";
+
+contract MockKyberDaoWithdrawFailed is EpochUtils {
+
+    uint public value;
+
+    constructor(uint _epochPeriod, uint _startBlock) public {
+        EPOCH_PERIOD = _epochPeriod;
+        START_BLOCK = _startBlock;
+    }
+
+    function handleWithdrawal(address, uint) public returns(bool) {
+        value++;
+        return false;
+    }
+}
