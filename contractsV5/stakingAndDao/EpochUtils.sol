2f40fee899a084f7a7049b7c7cd8891e4f70cd89
Le Xuan Manh <manhlx.300695@gmail.com>
2020-02-27 18:44:40 +0700

    Implementation of KyberStaking and KyberDAO (#647)
    * Add Staking and DAO contracts + tests
    
    * Using some constants in Helper function for test
    
    * Add log comment for failing test using default network
    
    * Fix getting result for concluded campaign
    
    * Improve KyberDAO, allow to set rewardbps and rebatebps when deploy instead of encoded brr data
    
    * Reduce min campaign duration to around 4 days as discussion
    
    * Increase default max options for a camp to 8 as discussion
    
    * Shorten error message in DAO
    
    * Allow create campaign for current and next epoch
    
    * Remove duplicated check for reward and rebate in constructor
    
    * Add more tests to check for test coverage, current status 100% (#651)
    
    * Add more tests to check for test coverage, current status 100%
    
    * Using expect event and revert for DAO tests
    
    * Using expect event and revert for Staking tests (#652)
    
    * Using expect event and revert for Staking tests
    
    * Add calculate gas consumption when camp has been concluded
    
    * Update names for epoch period, start block and min camp duration
    
    * Add comments to functions in Staking & DAO

---
+++ contractsV5/stakingAndDao/EpochUtils.sol
@@ -0,0 +1,21 @@
+pragma solidity 0.5.11;
+
+import "./SafeMath.sol";
+
+
+contract EpochUtils {
+    using SafeMath for uint;
+
+    uint public EPOCH_PERIOD_BLOCKS;
+    uint public FIRST_EPCOH_START_BLOCK;
+
+    function getCurrentEpochNumber() public view returns(uint) {
+        return getEpochNumber(block.number);
+    }
+
+    function getEpochNumber(uint blockNumber) public view returns(uint) {
+        if (blockNumber < FIRST_EPCOH_START_BLOCK || EPOCH_PERIOD_BLOCKS == 0) { return 0; }
+        // ((blockNumber - FIRST_EPCOH_START_BLOCK) / EPOCH_PERIOD_BLOCKS) + 1;
+        return ((blockNumber.sub(FIRST_EPCOH_START_BLOCK)).div(EPOCH_PERIOD_BLOCKS)).add(1);
+    }
+}
---
+++ contractsV5/stakingAndDao/mock/MockKyberDaoWithdrawFailed.sol
@@ -0,0 +1,18 @@
+pragma solidity 0.5.11;
+
+import "../EpochUtils.sol";
+
+contract MockKyberDaoWithdrawFailed is EpochUtils {
+
+    uint public value;
+
+    constructor(uint _epochPeriod, uint _startBlock) public {
+        EPOCH_PERIOD_BLOCKS = _epochPeriod;
+        FIRST_EPCOH_START_BLOCK = _startBlock;
+    }
+
+    function handleWithdrawal(address, uint) public returns(bool) {
+        value++;
+        return false;
+    }
+}
