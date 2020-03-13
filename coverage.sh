f246ad357f21294e053671838bdf975cd9ba614d
Desmond Ho <howeilun@hotmail.com>
2020-03-13 17:11:42 +0800

    remove duplicate line

--- coverage.sh
+++ coverage.sh
@@ -5,22 +5,7 @@ while getopts ":f" arg; do
   esac
 done
 
-#!/bin/sh
-while getopts ":f" arg; do
-  case $arg in
-    f) FILE=$OPTARG;;
-  esac
-done
-
 npx buidler compile --config ./coverageConfig/buidlerCoverageV4.js
-
-if [ -n "$FILE" ]
-then
-    npx buidler coverage --config ./buidlerCoverageSol5.js --testfiles $FILE
-else
-    npx buidler coverage --config ./buidlerCoverageSol5.js
-fi
-
 
 if [ -n "$FILE" ]
 then
