cc86cfdcecfa82bb050fd96d430baa1cbfc03141
Desmond <howeilun@hotmail.com>
2020-03-13 20:07:02 +0800

    Fix sol4 config file name change, missing colons (#736)
    * Fix coverage compile v4 line
    
    * remove duplicate line
    
    * ctrl C + crtl V failed me
    
    * Added missing colons
    
    * Added missing colon in tst.sh
    
    * Remove ganache instance
    
    * Remove extra line

--- coverage.sh
+++ coverage.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-while getopts ":f" arg; do
+while getopts "f:" arg; do
   case $arg in
     f) FILE=$OPTARG;;
   esac
 done
 
-npx buidler compile --config ./coverageConfig/buidlerCoverageV4.js
+npx buidler compile --config ./buidlerCoverageSol4.js
 
 if [ -n "$FILE" ]
 then
--- tst.sh
+++ tst.sh
@@ -1,26 +1,13 @@
 #!/bin/sh
-YLWBGBLK='\033[1;43;30m'
-NC='\033[0m'
 ALL=false
 
-while getopts ":a:f:k" arg; do
+while getopts ":a:f:" arg; do
   case $arg in
     a) ALL=true;;
     f) FILE=$OPTARG;;
-    k) FORK=$OPTARG;;
   esac
 done
 
-if [ -n "$FORK" ] 
-then
-  printf "${YLWBGBLK}Running fork: $FORK${NC}\n\n"
-  npx ganache-cli -e 1000 -k $FORK -q & 2> /dev/null
-else
-  npx ganache-cli -e 1000 -q & 2> /dev/null
-fi
-
-pid=$!
-sleep 3
 if [ -n "$FILE" ]; then
   npx buidler test --no-compile $FILE
 elif [ "$ALL" ]; then
@@ -29,4 +16,3 @@ elif [ "$ALL" ]; then
 else
   npx buidler test --no-compile --config ./buidlerCoverageSol5.js
 fi
-kill -9 $pid
