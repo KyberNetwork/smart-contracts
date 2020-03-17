e8046166fa5c656a822a302b32af9dc33f42e671
Ilan Doron <ilan.on.the.road@gmail.com>
2020-03-17 23:54:57 +0200

    fix inheritence

--- contractsSol5/mock/MaliciousFeeHandler.sol
+++ contractsSol5/mock/MaliciousFeeHandler.sol
@@ -1,13 +1,13 @@
 pragma solidity 0.5.11;
 
-import "../FeeHandler.sol";
+import "../KyberFeeHandler.sol";
 
 
-contract MaliciousFeeHandler is FeeHandler {
+contract MaliciousFeeHandler is KyberFeeHandler {
 
     constructor(address daoSetter, IKyberNetworkProxy _kyberNetworkProxy, address _kyberNetwork,
         IERC20 _knc, uint _burnBlockInterval)
-        public FeeHandler(daoSetter, _kyberNetworkProxy, _kyberNetwork, _knc, _burnBlockInterval)
+        public KyberFeeHandler(daoSetter, _kyberNetworkProxy, _kyberNetwork, _knc, _burnBlockInterval)
         {}
 
     function setTotalPayoutBalance(uint _amount) external {
