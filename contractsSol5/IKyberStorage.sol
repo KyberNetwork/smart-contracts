36e0eda2267b0da78d219cb2a66dad32f0dc15a8
ducquangkstn <ducquang.kstn@gmail.com>
2020-03-30 17:59:59 +0700

    add reserve id-address to storage

--- contractsSol5/IKyberStorage.sol
+++ contractsSol5/IKyberStorage.sol
@@ -14,9 +14,16 @@ contract IKyberStorage {
 
     function setDAOContract(IKyberDAO _kyberDAO) external returns (bool);
 
-    function addReserve(address reserve) external returns (bool);
+    function addReserve(address reserve, bytes8 reserveId)
+        external
+        returns (bool);
 
     function removeReserve(address reserve, uint256 startIndex)
         external
         returns (bool);
+
+    function convertReserveIdToAddress(bytes8[] reserveIds)
+        external
+        view
+        returns (address[] reserveAddresses);
 }
--- contractsSol5/KyberStorage.sol
+++ contractsSol5/KyberStorage.sol
@@ -19,6 +19,9 @@ contract KyberStorage is IKyberStorage {
     IKyberMatchingEngine[] internal matchingEngine;
     IKyberReserve[] internal reserves;
 
+    mapping(bytes8=>address[])      public reserveIdToAddresses;
+    mapping(address=>bytes8)        internal reserveAddressToId;
+
     IKyberNetwork public network;
 
     modifier onlyNetwork() {
@@ -77,8 +80,12 @@ contract KyberStorage is IKyberStorage {
         return (kyberDAO, feeHandler, matchingEngine);
     }
 
-    function addReserve(address reserve) external onlyNetwork returns (bool) {
+    function addReserve(address reserve, bytes8 reserveId) external onlyNetwork returns (bool) {
         reserves.push(IKyberReserve(reserve));
+        require(reserveAddressToId[reserve] == bytes8(0), "reserve has id");
+        require(reserveId != 0, "reserveId = 0");
+        reserveAddressToId[reserve] = reserveId;
+        return true;
     }
 
     function removeReserve(address reserve, uint256 startIndex)
@@ -96,6 +103,13 @@ contract KyberStorage is IKyberStorage {
         require(reserveIndex != 2**255, "reserve ?");
         reserves[reserveIndex] = reserves[reserves.length - 1];
         reserves.pop();
+        // remove reserve from mapping to address
+        require(reserveAddressToId[reserve] != bytes8(0), "reserve -> 0 reserveId");
+        bytes8 reserveId = reserveAddressToId[reserve];
+
+        reserveIdToAddresses[reserveId].push(reserveIdToAddresses[reserveId][0]);
+        reserveIdToAddresses[reserveId][0] = address(0);
+        reserveAddressToId[reserve] = bytes8(0);
     }
 
     /// @notice should be called off chain
@@ -104,4 +118,14 @@ contract KyberStorage is IKyberStorage {
     function getReserves() external view returns (IKyberReserve[] memory) {
         return reserves;
     }
+
+    function convertReserveIdToAddress(bytes8[] reserveIds)
+        external
+        view
+        returns (address[] reserveAddresses) {
+            reserveAddresses = new bytes8[](reserveIds.length);
+            for (uint i = 0; i < reserveIds.length; i++) {
+                reserveAddresses[i] = reserveIdToAddresses[reserveIds[i]][0];
+            }
+        }
 }
