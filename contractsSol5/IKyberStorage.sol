f1003834d9e10d76e63b7918d638aaed4893276d
Desmond Ho <howeilun@hotmail.com>
2020-04-02 15:42:46 +0800

    Modify layout

--- contractsSol5/IKyberStorage.sol
+++ contractsSol5/IKyberStorage.sol
@@ -15,51 +15,31 @@ contract IKyberStorage {
 
     function setDAOContract(IKyberDAO _kyberDAO) external returns (bool);
 
-    function addReserve(address reserve, bytes8 reserveId)
-        external
-        returns (bool);
+    function addReserve(address reserve, bytes8 reserveId) external returns (bool);
 
-    function removeReserve(address reserve, uint256 startIndex)
-        external
-        returns (bytes8 reserveId);
+    function removeReserve(address reserve, uint256 startIndex) external returns (bytes8 reserveId);
 
     function listPairForReserve(address reserve, IERC20 token, bool ethToToken, bool tokenToEth, bool add)
         external
         returns (bool);
 
-    function convertReserveAddresstoId(address reserve)
-        external
-        view
-        returns (bytes8 reserveId);
+    function convertReserveAddresstoId(address reserve) external view returns (bytes8 reserveId);
 
-    function convertReserveIdToAddress(bytes8 reserveId)
-        external
-        view
-        returns (address reserveAddress);
+    function convertReserveIdToAddress(bytes8 reserveId) external view returns (address reserveAddress);
 
     function convertReserveAddressestoIds(address[] calldata reserveAddresses)
-        external
-        view
+        external view
         returns (bytes8[] memory reserveIds);
 
     function convertReserveIdsToAddresses(bytes8[] calldata reserveIds)
-        external
-        view
+        external view
         returns (address[] memory reserveAddresses);
 
-    function getReservesPerTokenSrc(address token)
-        external
-        view
-        returns (bytes8[] memory reserveIds);
+    function getReservesPerTokenSrc(address token) external view returns (bytes8[] memory reserveIds);
 
-    function getReservesPerTokenDest(address token)
-        external
-        view
-        returns (bytes8[] memory reserveIds);
-
-    function addKyberProxy(address networkProxy, uint256 max_approved_proxies)
-        external
-        returns (bool);
+    function getReservesPerTokenDest(address token) external view returns (bytes8[] memory reserveIds);
+ 
+    function addKyberProxy(address networkProxy, uint256 max_approved_proxies) external returns (bool);
 
     function removeKyberProxy(address networkProxy) external returns (bool);
 
