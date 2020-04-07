5c026c2fa2b444ea2f17a6f7441ae435bcc508e1
Ilan Doron <ilan.on.the.road@gmail.com>
2020-04-07 22:27:35 +0300

    fix according to CR

--- contractsSol5/Dao/KyberFeeHandler.sol
+++ contractsSol5/Dao/KyberFeeHandler.sol
@@ -173,12 +173,6 @@ contract KyberFeeHandler is IKyberFeeHandler, Utils4, BurnConfigPermission {
         uint burnAmtWei
     );
 
-    struct WeiData {
-        uint rebate;
-        uint totalRebatePaid;
-        uint reward;
-    }
-    
 /// @dev handleFees function is called per trade on KyberNetwork. unless the trade is not involving any fees.
 /// @param rebateWallets a list of rebate wallets that are entitiled for fee with this trade.
 /// @param rebateBpsPerWallet percentage of rebate for each wallet, out of total rebate. BPS uints: 10000 = 100%
@@ -193,38 +187,31 @@ contract KyberFeeHandler is IKyberFeeHandler, Utils4, BurnConfigPermission {
         // handle platform fee
         feePerPlatformWallet[platformWallet] += platformFeeWei;
 
-        uint feeBRR = msg.value - platformFeeWei;
+        uint feeBRRWei = msg.value - platformFeeWei;
 
-        if (feeBRR == 0) {
+        if (feeBRRWei == 0) {
             // only platform fee paid
             totalPayoutBalance += platformFeeWei;
             emit FeeDistributed(platformWallet, platformFeeWei, 0, 0, rebateWallets, rebateBpsPerWallet, 0);
             return true;
         }
 
-        WeiData memory weiData;
+        uint rebateWei;
+        uint rewardWei;
         uint epoch;
+
         // Decoding BRR data
-        (weiData.reward, weiData.rebate, epoch) = getRRWeiValues(feeBRR);
+        (rewardWei, rebateWei, epoch) = getRRWeiValues(feeBRRWei);
 
-        uint totalRebateBps;
-        for (uint i = 0; i < rebateWallets.length; i++) {
-            // Internal accounting for rebates per reserve wallet (rebatePerWallet)
-            require(rebateWallets[i] != address(0), "rebate wallet 0");
-            rebatePerWallet[rebateWallets[i]] += weiData.rebate * rebateBpsPerWallet[i] / BPS;
-            // a few wei could be left out due to rounding down. so count only paid wei
-            weiData.totalRebatePaid += weiData.rebate * rebateBpsPerWallet[i] / BPS;
-            totalRebateBps += rebateBpsPerWallet[i];
-        }
-        require(totalRebateBps == BPS, "Not 100% rebate distributed.");
+        rebateWei = updateRebateValues(rebateWei, rebateWallets, rebateBpsPerWallet);
 
-        rewardsPerEpoch[epoch] += weiData.reward;
+        rewardsPerEpoch[epoch] += rewardWei;
 
         // update balance for rewards, rebates, fee
-        totalPayoutBalance += (platformFeeWei + weiData.reward + weiData.totalRebatePaid);
+        totalPayoutBalance += (platformFeeWei + rewardWei + rebateWei);
 
-        emit FeeDistributed(platformWallet, platformFeeWei, weiData.reward, weiData.rebate, rebateWallets, 
-            rebateBpsPerWallet, (feeBRR - weiData.reward - weiData.rebate));
+        emit FeeDistributed(platformWallet, platformFeeWei, rewardWei, rebateWei, rebateWallets, 
+            rebateBpsPerWallet, (feeBRRWei - rewardWei - rebateWei));
 
         return true;
     }
@@ -488,5 +475,26 @@ contract KyberFeeHandler is IKyberFeeHandler, Utils4, BurnConfigPermission {
         require(rateEthToKnc * BPS >= sanityEthToKncRate * (BPS - SANITY_RATE_DIFF_BPS), "Kyber Eth To KNC rate too low");
 
         return true;
+    }
+
+    function updateRebateValues(uint rebateWei, address[] memory rebateWallets, uint[] memory rebateBpsPerWallet) 
+        internal returns (uint totalRebatePaidWei) 
+    {
+
+        uint totalRebateBps;
+        uint walletRebateWei;
+
+        for (uint i = 0; i < rebateWallets.length; i++) {
+            require(rebateWallets[i] != address(0), "rebate wallet address 0");
+
+            walletRebateWei = rebateWei * rebateBpsPerWallet[i] / BPS;
+            rebatePerWallet[rebateWallets[i]] += walletRebateWei;
+
+            // a few wei could be left out due to rounding down. so count only paid wei
+            totalRebatePaidWei += walletRebateWei;
+            totalRebateBps += rebateBpsPerWallet[i];
+        }
+
+        require(totalRebateBps == BPS, "Not 100% rebate distributed.");
     }
 }
