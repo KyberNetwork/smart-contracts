0b7be89db08648ddd1ab698167938382b6f3320c
Mike <manhlx.300695@gmail.com>
2020-04-07 18:10:59 +0700

    Add vote function to DAO interface

--- contractsSol5/IKyberDAO.sol
+++ contractsSol5/IKyberDAO.sol
@@ -5,6 +5,8 @@ interface IKyberDAO {
     function EPOCH_PERIOD_BLOCKS() external view returns(uint);
     function FIRST_EPOCH_START_BLOCK() external view returns(uint);
 
+    function vote(uint campID, uint option) external;
+
     function handleWithdrawal(address staker, uint penaltyAmount) external returns(bool);
     function shouldBurnRewardForEpoch(uint epoch) external view returns(bool);
     function getLatestNetworkFeeData() external view returns(uint feeInBps, uint expiryBlockNumber);
