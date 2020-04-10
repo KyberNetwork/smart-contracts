232bf3011382174cadede07921b2492bcf2edb4b
Mike <manhlx.300695@gmail.com>
2020-04-10 12:28:37 +0700

    Fix network helper and network tests

--- contractsSol5/KyberNetwork.sol
+++ contractsSol5/KyberNetwork.sol
@@ -127,8 +127,8 @@ contract KyberNetwork is WithdrawableNoModifiers, Utils4, IKyberNetwork, Reentra
         external returns(bool)
     {
         onlyOperator();
-        require(kyberStorage.addReserve(reserve, reserveId));
-        require(matchingEngine.addReserve(reserveId, reserveType));
+        require(kyberStorage.addReserve(reserve, reserveId), "storage adds reserve failed");
+        require(matchingEngine.addReserve(reserveId, reserveType), "engine adds reserve failed");
         require(rebateWallet != address(0));
 
         reserveIdToAddress[reserveId] = reserve;
