6ceb72ffaa3bbf6befdd2d26f8aeac3b5ef6fe06
ductm54 <32837881+ductm54@users.noreply.github.com>
2020-05-19 15:15:02 +0700

    Add reports for CI and local compilation. Will report contract size changes and and trade gas consumption changes. (#947)
    Co-authored-by: Ilan Doron <ilan.on.the.road@gmail.com>

--- cmp.sh
+++ cmp.sh
@@ -3,3 +3,4 @@ export NODE_OPTIONS=--max-old-space-size=4096
 npx buidler compile
 npx buidler compile --config buidlerConfigSol5.js
 npx buidler compile --config buidlerConfigSol4.js
+node contractSizeReport.js
