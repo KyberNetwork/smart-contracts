eb7aef86a53d2baab0f4aee73e02d7c855f9ccae
Anton Buenavista <ay.buenavista@gmail.com>
2020-05-19 15:39:15 +0800

    Support additional error messages for off chain parse&&build hint functions. Errors: Duplicate reserves, split increasing order IDs, invalid reserve Ids.(#945)
    * Verifies the following for reserveIds for all external hint functions:
    
    * Invalid and unregistered reserveIds
    * Increasing order reserveIds for SPLITS
    * Duplicate reserveIds for MASK_IN, MASK_OUT
    
    Also enhanced tests and included tests for random hex fields for hints to parse.
    
    * Minor fix for network test due to new error handling in KyberHintHandler
    
    * Fix to storage.getReserveDetailsById for returning no data if reserveID doesn't exist, and fix for checking duplicate reserveIds
    
    Co-authored-by: Ilan Doron <ilan.on.the.road@gmail.com>

--- contracts/sol6/IKyberHint.sol
+++ contracts/sol6/IKyberHint.sol
@@ -6,13 +6,36 @@ import "./IKyberReserve.sol";
 interface IKyberHint {
     enum TradeType {MaskIn, MaskOut, Split}
     enum HintErrors {
-        NoError,
-        ReserveIdDupError,
-        ReserveIdEmptyError,
-        ReserveIdSplitsError,
-        SplitsNotEmptyError,
-        TotalBPSError
+        NoError, // Hint is valid
+        ReserveIdDupError, // duplicate reserveID found
+        ReserveIdEmptyError, // reserveIDs array is empty
+        ReserveIdSplitsError, // reserveIDs and splitBpsValues arrays do not have the same length
+        ReserveIdSequenceError, // reserveID sequence in array is not in increasing order
+        ReserveIdNotFound, // reserveID isn't registered or doesn't exist
+        SplitsNotEmptyError, // splitBpsValues is not empty for MaskIn or MaskOut trade type
+        TotalBPSError // total BPS for Split trade type is not 10000 (100%)
     }
+
+    function buildTokenToEthHint(
+        TradeType tokenToEthType,
+        bytes32[] calldata tokenToEthReserveIds,
+        uint256[] calldata tokenToEthSplits
+    ) external view returns (bytes memory hint);
+
+    function buildEthToTokenHint(
+        TradeType ethToTokenType,
+        bytes32[] calldata ethToTokenReserveIds,
+        uint256[] calldata ethToTokenSplits
+    ) external view returns (bytes memory hint);
+
+    function buildTokenToTokenHint(
+        TradeType tokenToEthType,
+        bytes32[] calldata tokenToEthReserveIds,
+        uint256[] calldata tokenToEthSplits,
+        TradeType ethToTokenType,
+        bytes32[] calldata ethToTokenReserveIds,
+        uint256[] calldata ethToTokenSplits
+    ) external view returns (bytes memory hint);
 
     function parseTokenToEthHint(bytes calldata hint)
         external
@@ -47,25 +70,4 @@ interface IKyberHint {
             IKyberReserve[] memory ethToTokenAddresses,
             uint256[] memory ethToTokenSplits
         );
-
-    function buildTokenToEthHint(
-        TradeType tokenToEthType,
-        bytes32[] calldata tokenToEthReserveIds,
-        uint256[] calldata tokenToEthSplits
-    ) external pure returns (bytes memory hint);
-
-    function buildEthToTokenHint(
-        TradeType ethToTokenType,
-        bytes32[] calldata ethToTokenReserveIds,
-        uint256[] calldata ethToTokenSplits
-    ) external pure returns (bytes memory hint);
-
-    function buildTokenToTokenHint(
-        TradeType tokenToEthType,
-        bytes32[] calldata tokenToEthReserveIds,
-        uint256[] calldata tokenToEthSplits,
-        TradeType ethToTokenType,
-        bytes32[] calldata ethToTokenReserveIds,
-        uint256[] calldata ethToTokenSplits
-    ) external pure returns (bytes memory hint);
 }
