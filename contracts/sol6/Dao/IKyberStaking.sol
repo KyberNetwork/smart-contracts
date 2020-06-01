729005bc45388e4b49b8e22d137dcd56d566c9d2
Desmond Ho <howeilun@hotmail.com>
2020-06-01 14:03:06 +0800

    Add more functions to interface

--- contracts/sol6/Dao/IKyberStaking.sol
+++ contracts/sol6/Dao/IKyberStaking.sol
@@ -28,6 +28,40 @@ interface IKyberStaking is IEpochUtils {
     function withdraw(uint256 amount) external;
 
     /**
+     * @notice return combine data (stake, delegatedStake, representative) of a staker
+     * @dev allow to get staker data up to current epoch + 1
+     */
+    function getStakerData(address staker, uint256 epoch)
+        external
+        view
+        returns (
+            uint256 stake,
+            uint256 delegatedStake,
+            address representative
+        );
+
+    function getStake(address staker, uint256 epoch) external view returns (uint256);
+
+    function getDelegatedStake(address staker, uint256 epoch) external view returns (uint256);
+
+    function getRepresentative(address staker, uint256 epoch) external view returns (address);
+
+    function getLatestStakerData(address staker)
+        external
+        view
+        returns (
+            uint256 stake,
+            uint256 delegatedStake,
+            address representative
+        );
+
+    function getLatestStakeBalance(address staker) external view returns (uint256);
+
+    function getLatestDelegatedStake(address staker) external view returns (uint256);
+
+    function getLatestRepresentative(address staker) external view returns (address);
+
+    /**
      * @notice return raw data of a staker for an epoch
      *         WARN: should be used only for initialized data
      *          if data has not been initialized, it will return all 0
