c23b14a353ca1160e3994acbfa276bfd4c077f3b
Ilan Doron <ilan.on.the.road@gmail.com>
2020-06-07 06:56:46 +0300

    Add missing functions in DAO interface files. In IKyberStaking and IEpochUtils.
    * Add to interface files functions that should be used by pool operator and user.
    
    * Add getStakerData  and getLatestStakerData to IKyberStaking.
    
    * Add getCurrentEpochNumber and getEpochNumber to IEpochUtils
    
    Co-authored-by: iland <iland@ilands-MacBook-Pro.local>

--- contracts/sol6/Dao/IKyberStaking.sol
+++ contracts/sol6/Dao/IKyberStaking.sol
@@ -28,6 +28,28 @@ interface IKyberStaking is IEpochUtils {
     function withdraw(uint256 amount) external;
 
     /**
+     * @notice return combine data (stake, delegatedStake, representative) of a staker
+     * @dev allow to get staker data up to current epoch + 1
+     */
+    function getStakerData(address staker, uint256 epoch)
+        external
+        view
+        returns (
+            uint256 stake,
+            uint256 delegatedStake,
+            address representative
+        );
+
+    function getLatestStakerData(address staker)
+        external
+        view
+        returns (
+            uint256 stake,
+            uint256 delegatedStake,
+            address representative
+        );
+
+    /**
      * @notice return raw data of a staker for an epoch
      *         WARN: should be used only for initialized data
      *          if data has not been initialized, it will return all 0
