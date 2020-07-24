84fbda1b9871a3bdf1552749609842163006d1e5
Desmond <howeilun@hotmail.com>
2020-07-24 20:07:57 +0800

    Add staking fuzz tests. Run semi random inputs to staking contract. Validate result as expected. (#963)
    * Added staking fuzz tests
    
    Save latest work
    
    Commit latest progress
    
    Fix comments in stakingActionsGenerator.js
    
    Fix contract name
    
    Fix some stuff from Mike's CR
    
    Commit latest progress
    
    Commit latest progress
    
    Add revert msgs
    
    Modify random generation, console log validity states, complete withdrawal checks
    
    Move random number generation to helper file, return if check fails
    
    Remove console.log
    
    * Minor fixes
    
    * Add CRLF
    
    * Use winston for logging into file
    
    * Round progress percentage

---
+++ scripts/randomNumberGenerator.js
@@ -0,0 +1,11 @@
+const BN = web3.utils.BN;
+module.exports.genRandomSeed = genRandomSeed;
+function genRandomSeed(base) {
+    return Math.floor(Math.random() * Math.random() * base) % base;
+}
+
+module.exports.genRandomBN = function(minBN, maxBN) {
+    let seed = new BN(genRandomSeed(1000000000000000));
+    // normalise seed
+    return (maxBN.sub(minBN).mul(seed).div(new BN(1000000000000000))).add(minBN);
+}
