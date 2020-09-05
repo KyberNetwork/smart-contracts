5bb04014c765e640d4da0c997075066f48b0dcd1
Tran Minh Duc <ductm54@gmail.com>
2020-09-05 20:18:42 +0000

    fix empty body

--- .travis/report.sh
+++ .travis/report.sh
@@ -4,15 +4,15 @@ readonly test_part=${TEST_PART:-}
 case "$test_part" in
 All)
     if [[ $TRAVIS_PULL_REQUEST ]]; then
-        sizeReport = `node contractSizeReport.js $TRAVIS_PULL_REQUEST_BRANCH`
-        gasReport = `node gasUsedReport.js $TRAVIS_PULL_REQUEST_BRANCH`
+        export sizeReport=$(node contractSizeReport.js $TRAVIS_PULL_REQUEST_BRANCH)
+        export gasReport=$(node gasUsedReport.js $TRAVIS_PULL_REQUEST_BRANCH)
         node travis/remove-all-bot-comment.js
         curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
-        -d "{\"body\": \"$sizeReport\"}" \
+        -d "{\"body\": \""$sizeReport"\"}" \
         "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
 
         curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
-        -d "{\"body\": \"$gasReport\"}" \
+        -d "{\"body\": \""$gasReport"\"}" \
         "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
     fi
     ;;
