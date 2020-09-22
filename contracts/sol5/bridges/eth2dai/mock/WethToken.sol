90d70be5e0b695a06a9cebf9af6fc5f3a239fd1f
Franco Victorio <victorio.franco@gmail.com>
2020-09-22 10:02:25 -0300

    Inline contracts/sol5/mock/Token.sol in WethToken and rename file

--- contracts/sol5/bridges/eth2dai/mock/WethToken.sol
+++ contracts/sol5/bridges/eth2dai/mock/WethToken.sol
@@ -1,12 +1,23 @@
-
 pragma solidity 0.5.11;
 
-import "../../../mock/Token.sol";
+import "../../../mock/StandardToken.sol";
 
-contract WethToken is Token {
 
-    constructor(string memory _name, string memory _symbol, uint _decimals) Token(_name, _symbol, _decimals) public {}
+contract WethToken is StandardToken {
+    string public name = "Test";
+    string public symbol = "TST";
+    uint256 public decimals = 18;
+    uint256 public INITIAL_SUPPLY = 10**(50 + 18);
 
+    constructor(string memory _name, string memory _symbol, uint _decimals) public {
+        totalSupply = INITIAL_SUPPLY;
+        balances[msg.sender] = INITIAL_SUPPLY;
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+    }
+
+    event Burn(address indexed _burner, uint256 _value);
     event Deposit(address indexed dst, uint wad);
 
     function deposit() public payable {
@@ -21,5 +32,19 @@ contract WethToken is Token {
         balances[msg.sender] -= wad;
         msg.sender.transfer(wad);
         emit Withdrawal(msg.sender, wad);
+    }
+
+    function burn(uint256 _value) public returns (bool) {
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        totalSupply = totalSupply.sub(_value);
+        emit Burn(msg.sender, _value);
+        emit Transfer(msg.sender, address(0x0), _value);
+        return true;
+    }
+
+    // save some gas by making only one contract call
+    function burnFrom(address _from, uint256 _value) public returns (bool) {
+        transferFrom(_from, msg.sender, _value);
+        return burn(_value);
     }
 }
