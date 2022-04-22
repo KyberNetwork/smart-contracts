3b13a3625c1ab0ffdf8fae209f9932efe4a6bc7a
Dhanushdhaari XP <61357926+Pnkjgot01@users.noreply.github.com>
2022-04-22 22:26:44 +0530

    Update FeeBurnerInterface.sol

--- contracts/sol4/FeeBurnerInterface.sol
+++ contracts/sol4/FeeBurnerInterface.sol
@@ -3,5 +3,5 @@ pragma solidity 0.4.18;
 
 interface FeeBurnerInterface {
     function handleFees (uint tradeWeiAmount, address reserve, address wallet) public returns(bool);
-    function setReserveData(address reserve, uint feesInBps, address kncWallet) public;
+    function setReserveData(address reserve, uint feesInBps, address nimWallet) public;
 }
