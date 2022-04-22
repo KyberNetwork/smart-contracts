f1e0956d74d9568218ff770f503e99b2497ea6b1
Dhanushdhaari XP <61357926+Pnkjgot01@users.noreply.github.com>
2022-04-22 23:41:22 +0530

    Update and rename IKyberDao.sol to INimbleDao.sol

--- contracts/sol6/INimbleDao.sol
+++ contracts/sol6/INimbleDao.sol
@@ -3,7 +3,7 @@ pragma solidity 0.6.6;
 import "./Dao/IEpochUtils.sol";
 
 
-interface IKyberDao is IEpochUtils {
+interface INimbleDao is IEpochUtils {
     event Voted(address indexed staker, uint indexed epoch, uint indexed campaignID, uint option);
 
     function getLatestNetworkFeeDataWithCache()
